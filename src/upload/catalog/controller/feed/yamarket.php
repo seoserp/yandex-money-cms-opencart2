3b438ac4f921ca758a5728323cae4fcab6d97989
ikarmanov <studentagpi@mail.ru>
2015-12-30 12:18:36 +0300

    fix picture (market)

--- src/upload/catalog/controller/feed/yamarket.php
+++ src/upload/catalog/controller/feed/yamarket.php
@@ -93,6 +93,7 @@ class ControllerFeedYamarket extends Controller {
 			$data['picture'] = array();
 			if ($product['minimum'] > 1)
 					$data['sales_notes'] = 'Минимальное кол-во для заказа: '.$product['minimum'];
+			if (isset($product['image'])) $data['picture'][] = $this->model_tool_image->resize($product['image'], 600, 600);
 			foreach ($this->model_catalog_product->getProductImages($data['id']) as $pic)
 				$data['picture'][] = $this->model_tool_image->resize($pic['image'], 600, 600);
 
