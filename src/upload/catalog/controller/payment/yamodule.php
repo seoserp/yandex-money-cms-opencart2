d07f4fdb8c2d6689e25faad0a8aa9d4c8e12b2bd
Ilya Bodrov <ibodrov1@gmail.com>
2017-08-18 13:36:49 +0300

    Правки по Code Review. Обновление архивов.

--- src/upload/catalog/controller/payment/yamodule.php
+++ src/upload/catalog/controller/payment/yamodule.php
@@ -566,7 +566,7 @@ class ControllerPaymentYamodule extends Controller
 
         $pairs = array_combine($keys, $order_info);
         $pairs = array_filter($pairs, function($items){
-            return !is_array($items);
+            return is_scalar($items);
         });
 
         return strtr($this->config->get('ya_fast_pay_description'), $pairs);
